6f3aca7948ab94986bfd04b9b42f9e55639978ac
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-06-06 09:03:26 +0200

    -

---
+++ MDM.CorsoVB.ConsoleApp/Module1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.IO
+'Imports System.printing
+Imports System.Net
+
+Module Module1
+
+    Sub Main()
+        ' Dichiarazione di una variabile
+        Dim età As Integer
+        Dim weight As Single = 34.187
+        Dim weight2 As Single = 55564564564534.188
+        Dim weight3 As Double = 8.5734985743895561E+37
+
+        Dim Altezza = 180.2, Peso = 78.5, Larghezza As Double
+
+        Larghezza = 55
+
+        Dim _Maggiorenne As Boolean = True
+        Dim anno_nascita As Integer = 1976
+
+        weight2 = weight3
+
+        'File.WriteAllText("Z:\\Prova.txt", "Igor")
+
+        'PascalCase (iniziali maiuscole di ciascuna parola)
+        ' Scrive il messaggio tra apici sullo schermo (NON fa nessuna pausa)
+        Console.WriteLine("Per favore, scrivi la tua età:")
+        età = Console.ReadLine()
+
+        ' camelCase
+        ' (iniziali maiuscole di ciascuna parola, tranne la prima)
+        Dim annoNascita As Integer = DateTime.Now.Year - età
+
+        'Console.WriteLine("Ok, hai ___ anni, perciò sei nato nel ___!")
+        'Console.WriteLine("Ok, hai " + età.ToString() + " anni, perciò sei nato nel " + annoNascita.ToString() + "!")
+        'Console.WriteLine(String.Format("Ok, hai {0} anni, perciò sei nato nel {1}!", età, annoNascita))
+
+        Dim messaggioUtente As String
+        messaggioUtente = String.Format("Se hai {0} anni, significa che sei nato nell'anno {1}", età, annoNascita)
+
+        Console.WriteLine(messaggioUtente)
+        File.WriteAllText("Z:\\Prova.txt", messaggioUtente)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ MDM.CorsoVB.ConsoleApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MDM.CorsoVB.ConsoleApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MDM.CorsoVB.ConsoleApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8de3e8d0-1c13-4d9a-95c8-09a2b347ce3f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
