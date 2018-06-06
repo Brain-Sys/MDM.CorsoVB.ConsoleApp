62aa3d4b9108b19145a6120bd784c9775f531f5c
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-06-06 09:04:32 +0200

    -

---
+++ MDM.CorsoVB.LogicaApplicazione/CalcoloAnno.vb
@@ -0,0 +1,16 @@
+﻿Public Class CalcoloAnno
+
+    Public Function DammiAnno(età As UShort) As UShort
+
+        If età <= 0 Then
+            Return 0
+        End If
+
+        Dim anno As UShort = DateTime.Now.Year - età
+
+        Return anno
+    End Function
+
+
+
+End Class
---
+++ MDM.CorsoVB.WindowsApp/MainWindow.xaml.vb
@@ -0,0 +1,45 @@
+﻿Imports MDM.CorsoVB.LogicaApplicazione
+
+Class MainWindow
+    Private Sub btnCalcolaAnnoNascita_Click(sender As Object, e As RoutedEventArgs) Handles btnCalcolaAnnoNascita.Click
+
+        Dim calcolatrice As New CalcoloAnno
+        Dim anno As UShort = calcolatrice.DammiAnno(txtEtà.Text)
+
+        If anno <> 0 Then
+            Dim messaggioUtente As String = String.Format("Se hai {0} anni, significa che sei nato nell'anno {1}", txtEtà.Text, anno)
+            lblRisultatoFinale.Content = messaggioUtente
+            MessageBox.Show(messaggioUtente)
+
+        Else
+
+            MessageBox.Show("Oops, qualcosa è andato storto!!!")
+
+        End If
+
+
+        '' Dichiarazione
+        '' U = Unsigned (solo numeri positivi)
+        '' Short = -32767 / +32768
+        'Dim annoNascita As UShort
+
+        'If txtEtà.Text <= 0 OrElse txtEtà.Text >= 130 Then
+        '    Beep()
+        '    MessageBox.Show("Spiacente, l'età non è valida!")
+        '    Return
+        'End If
+
+        '' Assegnazione
+        'annoNascita = DateTime.Now.Year - txtEtà.Text
+        'Dim messaggioUtente As String = String.Format("Se hai {0} anni, significa che sei nato nell'anno {1}", txtEtà.Text, annoNascita)
+        ''MessageBox.Show(messaggioUtente)
+
+        'lblRisultatoFinale.Content = messaggioUtente
+    End Sub
+
+    Private Sub txtEtà_KeyDown(sender As Object, e As KeyEventArgs) Handles txtEtà.KeyDown
+        If e.Key = Key.Return Then
+            btnCalcolaAnnoNascita_Click(Nothing, Nothing)
+        End If
+    End Sub
+End Class
